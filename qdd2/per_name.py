4a0b95d13f01462f598844d98b4a638b8e7a1654
JeonHyeJin14 <124948248+JeonHyeJin14@users.noreply.github.com>
2025-12-01 16:12:07 +0900

    Revert "Add all Google Drive files"
    This reverts commit 83c916817713b200c51fb06241f77596dbacf1da.

--- qdd2/per_name.py
+++
@@ -1,7 +0,0 @@
-"""
-Compatibility wrapper for person-name resolution.
-"""
-
-from qdd2.name_resolution import get_wikidata_english_name, resolve_person_name_en
-
-__all__ = ["get_wikidata_english_name", "resolve_person_name_en"]
